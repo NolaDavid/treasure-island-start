67de6d1057fe6ce8bfc7b70e1758d4035cb92712
NolaDavid <dguillory62888@gmail.com>
2021-07-29 01:21:51 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,45 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+choice1 = input('You come to a crossing, where do you go? Type "Left" or "Right". \n').lower()
+if choice1 == 'left':
+  choice2 = input('You have come to a lake, with an island in the middle. Type "wait" to wait for a boat. Type "Swim" if you want to swim to the island. \n').lower()
+  if choice2 == 'wait':
+    choice3 = input('As you get to the island, you see three colored doors. Which do you choose? Red, Blue, or Yellow \n').lower()
+    if choice3 == 'yellow':
+      print('WINNER!!!')
+    else:
+      print('You walked into an ambush!!GAME OVER!!!!')
+  else:
+    print('You were attacked by a Hippo, GAME OVER')
+else:
+  print('You fell into a trap, GAME OVER')
+
+
+
+
+
+
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
